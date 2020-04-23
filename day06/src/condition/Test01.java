7621aa5f4aa5ea23ce8fc54262a38213cfdf1c54
user1 <user1@DESKTOP-QD9ND2F>
2020-04-23 10:47:13 +0900

    if사용 홀짝 구하기

---
+++ day06/src/condition/Test01.java
@@ -0,0 +1,31 @@
+package condition;
+import java.lang.*;
+public class Test01 {
+	public static void main(String[] args) {
+		//n이 홀 짝 확인 ..  true  false 말고 글자로 알고싶다.
+		//보여주고 싶은 결과는 2종류중 하나 .
+		
+		
+		
+		
+		int n = 15;
+		    
+		if(n %2 == 0){
+		System.out.println("짝수입니다.");
+		}
+		
+		if(n  %2 != 0)
+		{
+			System.out.println("홀수입니다.");
+		}
+		
+	
+	// if(n  %2 != 0) 뿐만 아니라 if(n  %2 == 1) 도 가능하다. 
+		
+		
+		
+		
+	
+	}
+
+}
