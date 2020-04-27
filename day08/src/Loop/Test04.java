663462ad72025ca9f503b320368b99c371b605d3
627sung <627sung@naver.com>
2020-04-27 11:38:23 +0900

    주사위를 5번 던져보자

--- day08/src/Loop/Test04.java
+++ day08/src/Loop/Test04.java
@@ -5,22 +5,21 @@ import java.util.Random;
 public class Test04 {
 	public static void main (String [] args) {
 		
-		//주사위를 5번 던져보세요 
+		//주사위를 5번 던져보세요    안부터 만들고 겉으로 감싸라. 
+		
+		
+		//1번의 주사위 결과 출력 코드 
 		
 		Random r = new Random();
 		
-		
-		int i = 0;
-		while(i<5) {
+		int n = 0;
+		while(n<5) {
+			
 			int dice = r.nextInt(6)+1;
-			System.out.println(dice);
-			i++;
+			System.out.println("주사위의 눈 "+dice);
+			n++;
+		
 		}
-		
-		
-		
-		
-		
-		
+
 	}
 }
