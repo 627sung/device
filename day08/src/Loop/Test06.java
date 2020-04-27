4fde356ff1abb6096c978ccbadd8c93615219bde
627sung <627sung@naver.com>
2020-04-27 12:05:49 +0900

    1~1000까지의 합

---
+++ day08/src/Loop/Test06.java
@@ -0,0 +1,19 @@
+package Loop;
+
+public class Test06 {
+
+	public static void main(String[] args) {
+
+		
+		int n = 0 ;
+		int total =0;
+		while (n <=1000) {
+			total +=n;
+			n++;
+			
+		}
+		System.out.println(total);
+
+	}
+
+}
