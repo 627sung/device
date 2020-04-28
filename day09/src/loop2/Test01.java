3c2bd567afe66cf174491f4f13b4308628049673
627sung <627sung@naver.com>
2020-04-28 13:17:07 +0900

    do while 예제

---
+++ day09/src/loop2/Test01.java
@@ -0,0 +1,32 @@
+package loop2;
+
+import java.util.Scanner;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// do - while문 (do문)
+		// 최소 1번은 실행한다. 선실행 후검사를 실행하는 구문 ..
+		Scanner sc =new Scanner(System.in);
+
+		// 구문이 어려워서 최근에는 잘 안쓰는 추세임 
+		
+		
+		int score;
+		do {
+		System.out.println("시험성적입력");
+		 score = sc .nextInt();
+		}
+		while(score <0 || score >100);{
+			
+		}
+		
+		
+		sc.close();
+		
+		
+		
+		
+	}
+
+}
