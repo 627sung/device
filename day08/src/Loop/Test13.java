5dcd5587835c38c786961b5a5d0da858d1f76854
627sung <627sung@naver.com>
2020-04-28 10:04:08 +0900

    업다운

--- day08/src/Loop/Test13.java
+++ day08/src/Loop/Test13.java
@@ -9,22 +9,24 @@ public class Test13 {
 		Random r = new Random();
 		Scanner sc = new Scanner(System.in);
 		// 설정해야할것
-		int comNum = r.nextInt(1000) + 1;   //1~1000까지 랜덤으로 골라짐
+		int comNum = r.nextInt(1000) + 1; // 1~1000까지 랜덤으로 골라짐
 		System.out.println("컴퓨터가 문제를 설정했어요 맞춰보십시요");
 		int count = 0;
 		while (true) {
 			int inputNum = sc.nextInt(); // 사용자가 넣는 숫자
 			count++;
-			if (comNum == inputNum) {      //정답시
+			if (comNum == inputNum) { // 정답시
 				System.out.println("정답!  프로그램종료");
 				break;
 
 			} else if (comNum > inputNum) {
 				System.out.println("숫자가 더 커야합니다.");
-			} else if (comNum < inputNum)
+			} else {
 				System.out.println("숫자는 더 작아요 ");
-
+			 }
+			
 		}
+		sc.close();
 		System.out.println("총" + count + "번 만에 맞추셨습니다.");
 	}
 
---
+++ day08/src/Loop/Test13_T.java
@@ -0,0 +1,13 @@
+package Loop;
+
+import java.util.Random;
+
+public class Test13_T {
+
+	public static void main(String[] args) {
+	
+		
+		
+	}
+	
+}
