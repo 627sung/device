c01bdeffa1a042cc3b36d10b0fefd69316bce875
627sung <627sung@naver.com>
2020-04-29 13:26:54 +0900

    1000까지 소수뽑기

---
+++ day09/src/loop2/Test09_up.java
@@ -0,0 +1,28 @@
+package loop2;
+
+import java.util.Scanner;
+
+public class Test09_up {
+
+	public static void main(String[] args) {
+		
+		
+			int n = 1000;
+		for(int j =2; j <n; j++)	{
+			int count =0;
+			for(int i = 1; i <=j; i++  ) {
+				if(j% i ==0) {
+					 count++;
+					 break;
+				 }// if문 괄호
+					
+			} //for문 괄호 (  소수이면 카운트 2시키는  for 문)
+		
+				if(count ==0) {
+					System.out.println(j);
+				}
+			
+		}	
+		
+	}
+}
