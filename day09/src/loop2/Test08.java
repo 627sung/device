93a9eb58c5100a22400e00649b47d56c2c889147
627sung <627sung@naver.com>
2020-04-29 13:27:08 +0900

    피보나치 수열

---
+++ day09/src/loop2/Test08.java
@@ -0,0 +1,29 @@
+package loop2;
+import java .lang.*;
+public class Test08 {
+
+	public static void main(String[] args) {
+			//피보나치 수열 황금비 계산시 사용 
+ 		// 1   1 2 3 5 8 13 21 34 55 89 144 
+	//[1] 1,2번째 자리가 1이다.
+	
+			int a = 1;
+			int b = 1;
+			int c = a+b;
+			
+			a = b;
+			b = c;
+			c = a + b;
+	
+		
+		
+	
+		
+	
+	
+	}
+
+		 }
+		
+  	
+
---
+++ day09/src/loop2/Test08_T.java
@@ -0,0 +1,31 @@
+package loop2;
+import java .lang.*;
+public class Test08_T {
+
+	public static void main(String[] args) {
+			//피보나치 수열 황금비 계산시 사용 
+ 		// 1   1 2 3 5 8 13 21 34 55 89 144  30번째832040
+	//[1] 1,2번째 자리가 1이다.
+	int a = 1;
+	int b = 1;
+	//[2] 3번째 자리는 1번째와 2번째 자리의 합이다. (규칙)
+	int c = a+b;
+	System.out.println("c="+c);
+	
+	a = b;
+	b = c ;
+	c= a + b;
+	
+	
+		
+		
+		//앞자리와 앞앞자리만 구한다. 
+		
+	
+	
+	}
+
+		 }
+		
+  	
+
