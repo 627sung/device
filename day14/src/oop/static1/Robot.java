3ba42f91c69e5158ec6a366a7b4a25c8128cdbeb
627sung <627sung@naver.com>
2020-05-08 15:23:28 +0900

    스태틱 이용 더하기 연산해보기 예제

---
+++ day14/src/oop/static1/Robot.java
@@ -0,0 +1,39 @@
+package oop.static1;
+
+public class Robot {
+	private int a ; /// 첫번재값
+	private int b ;//두번재값
+	private int c;// 결과값
+		
+	public void setA(int a){
+		this.a=a;
+	}
+	public int getA() {
+		return a;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+	public int getB() {
+		return b;
+	}
+	
+	public void setC(int c) {
+		this.c = c;
+	}
+	public int getC() {
+		return c;
+	}
+
+	//public Robot()[] // 기본생성자;
+
+
+
+
+
+
+
+
+
+}
---
+++ day14/src/oop/static1/Test01.java
@@ -0,0 +1,20 @@
+package oop.static1;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+			//로봇을 이용한 더하기 연산
+		
+		Robot rb  = new Robot();
+		rb.setA(10);
+		rb.setB(20);
+		rb.setC(rb.getA()+rb.getB());
+		
+		System.out.println(rb.getC());
+		
+		
+		
+		
+	}
+
+}
