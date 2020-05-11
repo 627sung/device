3f159ef40935d95b55da0429c09eeea58647703a
627sung <627sung@naver.com>
2020-05-11 16:15:55 +0900

    상속으로 날로먹기

---
+++ day15/src/oop/inherit3/Sample.java
@@ -0,0 +1,9 @@
+package oop.inherit3;
+
+import javax.swing.JColorChooser;
+
+//public class Sample extends JFrame {//창
+//public class Sample extends JFileChooser {//창
+public class Sample extends JColorChooser {//창
+
+}
---
+++ day15/src/oop/inherit3/Test01.java
@@ -0,0 +1,9 @@
+package oop.inherit3;
+
+public class Test01 {
+	public static void main(String[] args) {
+		Sample s=new Sample();
+		s.showDialog(null,null,null);
+		
+	}
+}
