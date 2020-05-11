44deaf42347f85c312c881e2cb54cd27fff1415d
627sung <627sung@naver.com>
2020-05-11 15:59:13 +0900

    파이널 써보기

---
+++ day14/src/oop/final1/Test01.java
@@ -0,0 +1,24 @@
+package oop.final1;
+
+public class Test01 {
+     
+	public static void main(String[] args) {
+      // final 마지막 ,더이상 ooo을 못하게 하겠다는 의미이다. 
+		// final을 붙이면 한번만 넣게 해준다 
+		
+	final	int a = 10;
+		//a=20;
+		//a=30;
+		
+		
+		System.out.println("a = "+a);
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
