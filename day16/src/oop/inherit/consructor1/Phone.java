b6fbbcf032f2c6e571ef597b9d48d524916c0306
627sung <627sung@naver.com>
2020-05-14 11:13:08 +0900

    생성자 1 예제

--- day16/src/oop/inherit/consructor1/Phone.java
+++ day16/src/oop/inherit/consructor1/Phone.java
@@ -1,5 +1,12 @@
 package oop.inherit.consructor1;
-
+//상위 클래스(추상적인..)
 public class Phone {
-
+	protected String number;
+	protected String telecom;
+	
+	//생성자를 통해서 Phone의 필수 항목들을 초기화
+	public Phone(String number, String telecom) {
+		this.number = number;
+		this.telecom = telecom;
+	}
 }
--- day16/src/oop/inherit/consructor1/Test01.java
+++ day16/src/oop/inherit/consructor1/Test01.java
@@ -1,5 +1,8 @@
 package oop.inherit.consructor1;
 
 public class Test01 {
-
+	public static void main(String[] args) {
+//		GalaxyS20 생성
+		GalaxyS20 a = new GalaxyS20("010-1212-3434", "LG", 256);
+	}
 }
