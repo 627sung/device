baa66006cbdad9e27afaa8e5b1e9635e8989ecb1
627sung <627sung@naver.com>
2020-05-14 11:12:27 +0900

    다중상속 인터페이스 예제

---
+++ day16/src/oop/inherit/multi1/Korean.java
@@ -0,0 +1,8 @@
+package oop.inherit.multi1;
+
+public interface Korean {
+ 
+	void speak();
+ 
+ 
+}
---
+++ day16/src/oop/inherit/multi1/Teacher.java
@@ -0,0 +1,18 @@
+package oop.inherit.multi1;
+//강사 인터페이스 
+//-다중상속 전용 클래스 (클래스의 변형)
+//클래스에서 다중상속시 문제가 되는 요소들이 모두 사라졌음
+public interface Teacher {
+//필드 , 메소드 , 생성자 
+ 	 /*public static final*/ int a=10;
+ 	/*public static final*/  String subject ="자바";
+	
+	
+	public /*abstract*/void teach(); 
+	
+	
+	
+	
+	
+	
+}
