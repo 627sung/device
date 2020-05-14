055086f3d8ea50a6290f53d61ba2dc9d9f3731eb
627sung <627sung@naver.com>
2020-05-14 11:13:32 +0900

    추상화3

---
+++ day16/src/oop/inherit/abstract3_1/Phone.java
@@ -0,0 +1,7 @@
+package oop.inherit.abstract3_1;
+
+public abstract class Phone {
+	public abstract void call() ;
+	public abstract void sms() ;
+	public abstract void camera();
+}
---
+++ day16/src/oop/inherit/abstract3_1/V60.java
@@ -0,0 +1,15 @@
+package oop.inherit.abstract3_1;
+
+public class V60 extends Phone{
+	public void call() {
+		System.out.println("v60의 전화 기능 실행");
+	}
+
+	public void sms() {
+		System.out.println("v60의 문자 기능 실행");
+	}
+
+	public void camera() {
+		System.out.println("v60의 카메라 기능 실행");
+	}
+}
