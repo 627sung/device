f8ad2b931ea17ae2c6be831ec15428703349558f
627sung <627sung@naver.com>
2020-06-16 09:58:35 +0900

    dao 기본구현

--- home/src/home/beans/dao/BoardDao.java
+++ home/src/home/beans/dao/BoardDao.java
@@ -13,9 +13,9 @@ public class BoardDao {
 
 	//연결 메소드
 	public Connection getConnection() throws Exception{
-	Class.forName("oracle.jdbc.OracleDriver");
-	return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "c##kh", "c##kh");
-	}
+		Class.forName("oracle.jdbc.OracleDriver");
+		return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "c##kh", "c##kh");
+		}
 	
 	public List<BoardDto> get() throws Exception{
 		Connection con = getConnection();
--- home/src/home/beans/dto/BoardDto.java
+++ home/src/home/beans/dto/BoardDto.java
@@ -1,112 +1,64 @@
 package home.beans.dto;
 
-public class BoardDto {
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-	private int board_no;
-	private String board_head;
-	private String board_title;
-	private String board_writer;
-	private String board_content;
-	private String board_date ;
-	private int board_read;
-	
-	
+public class BoardDto {
+	private int board_no, board_read;
+	private String board_head, board_title, board_writer, board_content, board_date;
 	public BoardDto() {
 		super();
 	}
-
-
-	public BoardDto(int board_no, String board_head, String board_title, String board_writer, String board_content,
-			String board_date, int board_read) {
-		super();
-		this.board_no = board_no;
-		this.board_head = board_head;
-		this.board_title = board_title;
-		this.board_writer = board_writer;
-		this.board_content = board_content;
-		this.board_date = board_date;
-		this.board_read = board_read;
+	//추가 : ResultSet을 BoardDto로 변환하는 생성자
+	public BoardDto(ResultSet rs) throws SQLException {
+		this.setBoard_no(rs.getInt("board_no"));
+		this.setBoard_head(rs.getString("board_head"));
+		this.setBoard_title(rs.getString("board_title"));
+		this.setBoard_content(rs.getString("board_content"));
+		this.setBoard_writer(rs.getString("board_writer"));
+		this.setBoard_date(rs.getString("board_date"));
+		this.setBoard_read(rs.getInt("board_read"));
 	}
-
-
 	public int getBoard_no() {
 		return board_no;
 	}
-
-
 	public void setBoard_no(int board_no) {
 		this.board_no = board_no;
 	}
-
-
-	public String getBoard_head() {
-		return board_head;
-	}
-
-
-	public void setBoard_head(String board_head) {
-		this.board_head = board_head;
-	}
-
-
-	public String getBoard_title() {
-		return board_title;
-	}
-
-
-	public void setBoard_title(String board_title) {
-		this.board_title = board_title;
-	}
-
-
-	public String getBoard_writer() {
-		return board_writer;
-	}
-
-
-	public void setBoard_writer(String board_writer) {
-		this.board_writer = board_writer;
-	}
-
-
-	public String getBoard_content() {
-		return board_content;
-	}
-
-
-	public void setBoard_content(String board_content) {
-		this.board_content = board_content;
-	}
-
-
-	public String getBoard_date() {
-		return board_date;
-	}
-
-
-	public void setBoard_date(String board_date) {
-		this.board_date = board_date;
-	}
-
-
 	public int getBoard_read() {
 		return board_read;
 	}
-
-
 	public void setBoard_read(int board_read) {
 		this.board_read = board_read;
 	}
-
-
-	
-	
-	
-	
-
-		
-		
-		
-	
-	
+	public String getBoard_head() {
+		return board_head;
+	}
+	public void setBoard_head(String board_head) {
+		this.board_head = board_head;
+	}
+	public String getBoard_title() {
+		return board_title;
+	}
+	public void setBoard_title(String board_title) {
+		this.board_title = board_title;
+	}
+	public String getBoard_writer() {
+		return board_writer;
+	}
+	public void setBoard_writer(String board_writer) {
+		this.board_writer = board_writer;
+	}
+	public String getBoard_content() {
+		return board_content;
+	}
+	public void setBoard_content(String board_content) {
+		this.board_content = board_content;
+	}
+	public String getBoard_date() {
+		return board_date;
+	}
+	public void setBoard_date(String board_date) {
+		this.board_date = board_date;
+	}
 }
